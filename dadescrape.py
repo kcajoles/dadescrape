4007a1e9d5f9dcf7109ad29f6047e91143d4dfe3
kcajoles <kcajoles@live.com>
2019-09-10 01:13:34 +0800

    Initial commit

---
+++ dadescrape.py
@@ -0,0 +1,40 @@
+# Process Flow
+
+# 1) Read the Folio numbers in /home/kit/Downloads/Public-Certlist Paid_Unpd.xlsx
+
+# 2) Loop each folio number and enter it on the website ('https://www8.miamidade.gov/Apps/PA/propertysearch/#/')
+
+# 3) Use Selenium to access the website above and search each folio numbers and submit
+
+# 4) Parse data using BS4
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+from bs4 import BeautifulSoup
+
+
+# Read Data from Excel Column Account Number
+df = pd.read_excel('/home/kit/Downloads/Public-CertlistPaid_Unpd.xlsx', sheet_name='Sheet1') # getting FileNot Found Error
+
+
+# Selenium will access the website and enter the folio numbers to search page
+for folio in df['Account Number']:
+    driver = webdriver.Chrome()
+    driver.get ('https://www8.miamidade.gov/Apps/PA/propertysearch/#/')
+    driver.find_element_by_id('t-folio').click()
+    search_bar = driver.find_element_by_id('search_box')
+    search_bar.send_keys(folio)
+    search_bar.send_keys((Keys.RETURN))
+    element = WebDriverWait(driver, 6).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="property_info"]')))
+
+# Scrape all needed info
+    scrape_page = driver.page_source
+    soup = BeautifulSoup(scrape_page, 'lxml')
+
